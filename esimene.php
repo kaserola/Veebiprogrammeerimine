4eee9fe11a6ecccb81d7c4c2e9dbb9856b1148ab
Roland Kasenurm <kaserola@users.noreply.github.com>
2017-09-11 11:37:20 +0300

    uuendatud

--- esimene.php
+++ esimene.php
@@ -1,3 +1,26 @@
+<?php
+    //Muutujad
+	$myName = "Roland";
+	$myFamilyName = "Kasenurm";
+	$practiceStarted = "2017-09-11 8.15";
+	
+	//echo strtotime($practiceStarted);
+	//echo strtotime("now");
+	//$timePassed = strtotime("now") - strtotime($practiceStarted);
+	//echo $timePassed;
+	
+	$hourNow = date("H");
+	$partOfDay = "";
+	
+	if ($hourNow < 8){
+	$partOfDay = "varane hommik.";
+	}
+	if ($hourNow >= 8){
+	$partOfDay = "koolipäev";
+	}
+	
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,7 +33,17 @@
    
    <?php
        echo "<p>Täna on vastik ilm!<p>";
+	   echo "<p>Täna on ";
+	   echo date("d.m.Y");
+	   echo ".</p>";
+	   echo "<p>Lehe laadimise hetkel oli kell: " .date("H:i:s") ."</p>";
+	   echo "Praegu on " .$partOfDay .".";
    ?>
-
+   <p> PHP käivitatakse lehe laadimisel ja siis tehakse
+   kogu töö ära. Hiljem, kui vaja midagi jälle
+   "kalkuleerida" siis laetakse kogu leht uuesti.</p>
+   <?php 
+       echo "<p>Lehe autori täisnimi on: " .$myName ." " .$myFamilyName .".</p>";
+   ?>
 </body>
 </html>
