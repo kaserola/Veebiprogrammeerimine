61e584992c642c37e453a6216eeecff5fa83a7e5
Roland Kasenurm <kaserola@users.noreply.github.com>
2017-09-11 10:37:00 +0300

    HTML koodile lisatud esimene php osa

---
+++ esimene.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <title>Roland Kasenurm veebiprogemise asjad</title>
+</head>
+<body>
+   <h1>Roland Kasenurm</h1>
+   <p>See veebileht on loodud veebiprogrammerimis kursusel ning ei sisalda mingisugust tõsiseltvõetavat sisu.</p>
+   
+   <?php
+       echo "<p>Täna on vastik ilm!<p>";
+   ?>
+
+</body>
+</html>
